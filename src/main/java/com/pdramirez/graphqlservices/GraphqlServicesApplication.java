311d4e8501818d44a61f6f9babd48784a56c6969
Paul <pdramirez.h@gmail.com>
2023-02-10 12:02:42 -0500

    Init Graphql Server

---
+++ src/main/java/com/pdramirez/graphqlservices/GraphqlServicesApplication.java
@@ -0,0 +1,13 @@
+package com.pdramirez.graphqlservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pdramirez/graphqlservices/GraphqlServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdramirez.graphqlservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
